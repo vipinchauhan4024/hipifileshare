30107e59cd756ebacf9319fa61a1a8ec624c48ad
vipin chauhan <vipin.chauhan4026@gmail.com>
2022-04-11 19:48:20 +0530

    Update HttpTriggerFunction.java

--- hipi-azure-function/src/main/java/volvo/com/hipi/functions/HttpTriggerFunction.java
+++ hipi-azure-function/src/main/java/volvo/com/hipi/functions/HttpTriggerFunction.java
@@ -8,7 +8,6 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
 import java.util.Optional;
 
 /**
