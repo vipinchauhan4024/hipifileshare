0f63c2506d69338c000d98c521a9de0c40ed3c49
a214034 <vipin.chauhan@volvo.com>
2022-06-24 17:15:50 +0530

    con string used

--- hipi-azure-function/src/main/java/volvo/com/hipi/helper/AzureBlobStorageClient.java
+++ hipi-azure-function/src/main/java/volvo/com/hipi/helper/AzureBlobStorageClient.java
@@ -1,5 +1,6 @@
 package volvo.com.hipi.helper;
 
+import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
@@ -12,29 +13,31 @@ public class AzureBlobStorageClient {
     private static DataLakeServiceClient dataLakeServiceClient;
     private static String accountName ="hipifilesadlgen2";
     private static String accountKey ="kBvu9bVMY53Pi+4u8RAxTPOVmxo/G2/4rtsiZZBgh36+p+637q1IeFL/gH2V52wd2m9aEcF6Qcux2c9M1haNWA==";
-   // private static String accountName ="hipideveuwst01";
+    // private static String accountName ="hipideveuwst01";
     //private static String accountKey ="2J91Z3i5eQK8uURipAnKLo4PbyKthtb/+SV+aD9VyQk352WXL/EVMT+d3tqrvPnkUGJlyoU2nvATUKp2Gz3CnQ==";
+    private static String connectStr = "DefaultEndpointsProtocol=https;AccountName=hipifilesadlgen2;AccountKey=kBvu9bVMY53Pi+4u8RAxTPOVmxo/G2/4rtsiZZBgh36+p+637q1IeFL/gH2V52wd2m9aEcF6Qcux2c9M1haNWA==;EndpointSuffix=core.windows.net";
+
 
     public static BlobServiceClient  getAzureBlobStorageClient(){
         if(blobServiceClient == null){
             System.out.println( "connecting to azure blob" );
             String connectStr = "DefaultEndpointsProtocol=https;AccountName=hipifilesadlgen2;AccountKey=kBvu9bVMY53Pi+4u8RAxTPOVmxo/G2/4rtsiZZBgh36+p+637q1IeFL/gH2V52wd2m9aEcF6Qcux2c9M1haNWA==;EndpointSuffix=core.windows.net";
-           // String connectStr = "DefaultEndpointsProtocol=https;AccountName=hipideveuwst01;AccountKey=2J91Z3i5eQK8uURipAnKLo4PbyKthtb/+SV+aD9VyQk352WXL/EVMT+d3tqrvPnkUGJlyoU2nvATUKp2Gz3CnQ==;EndpointSuffix=core.windows.net";
+            // String connectStr = "DefaultEndpointsProtocol=https;AccountName=hipideveuwst01;AccountKey=2J91Z3i5eQK8uURipAnKLo4PbyKthtb/+SV+aD9VyQk352WXL/EVMT+d3tqrvPnkUGJlyoU2nvATUKp2Gz3CnQ==;EndpointSuffix=core.windows.net";
 
             blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
         }
         return blobServiceClient;
     }
-    
+
     static public DataLakeServiceClient GetDataLakeServiceClient(){
         if (dataLakeServiceClient != null){
-        	return dataLakeServiceClient;
+            return dataLakeServiceClient;
         }
-    	
+
         StorageSharedKeyCredential sharedKeyCredential =
-            new StorageSharedKeyCredential(accountName, accountKey);
+                // new StorageSharedKeyCredential(accountName, accountKey);
 
-
+                StorageSharedKeyCredential.fromConnectionString(connectStr);
         DataLakeServiceClientBuilder builder = new DataLakeServiceClientBuilder();
 
         builder.credential(sharedKeyCredential);
